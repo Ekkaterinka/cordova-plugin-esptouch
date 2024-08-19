df3bdec93ae787abb768aad7507c98ed38317b9a
Ekaterina <e.belova@microline.ru>
2024-08-19 09:16:18 +0300

    fix: edit import

--- src/ios/wifi.h
+++ src/ios/wifi.h
@@ -1,7 +1,7 @@
 #import <Cordova/CDVPlugin.h>
 #import <Cordova/CDVPluginResult.h>
 #import <CoreLocation/CoreLocation.h>
-#import <reachability.h>
+#import "reachability.h"
 #import "ESPTools.h"
 
 @interface wifi : CDVPlugin
